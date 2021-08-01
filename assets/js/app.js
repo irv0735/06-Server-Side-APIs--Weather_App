83a2663d4c0faa3c574159d20997fb4a87fe4724
Nathan Irvin <65375604+irv0735@users.noreply.github.com>
2021-07-31 19:41:17 -0600

    began creating additional functions to capture the received data and set up the render calls

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,14 @@
+const currentWeather = document.querySelector("#current-weather");
+const searchHistory = document.querySelector("#search-history");
+const forecastTable = document.querySelector("#forecast-table");
+
 const openWeatherAPIkey = "&appid=1057847a31bdd1cca5308fb38057c338";
 const openWeatherOneAPIurl = "https://api.openweathermap.org/data/2.5/onecall?";
 const openWeatherGeoAPIurl = "http://api.openweathermap.org/geo/1.0/direct?q=";
 const geoLimit = "&limit=1";
 const adjustmentsUrl = "&exclude=minutely,hourly&units=imperial";
+let searchTerm = "";
+let today = "";
 let previousSearch = [];
 
 //lat={lat}&lon={lon}
@@ -12,8 +18,9 @@ function getGeoData(location) {
     .then(function(response) {
         if (response.ok) {
             response.json().then(function (data) {
-                previousSearch.push(data[0].name);
-                console.log(data[0].lat, data[0].lon);
+                if (previousSearch.unshift(data[0].name) > 10) {
+                    previousSearch.pop();
+                }
                 getWeatherData(data[0].lat, data[0].lon);
             });
         } else {
@@ -29,8 +36,13 @@ function getWeatherData(lat, lon) {
     fetch(openWeatherOneAPIurl + "lat=" + lat + "&lon=" + lon + adjustmentsUrl + openWeatherAPIkey)
     .then(function(response) {
         if (response.ok) {
+            // send to localStorage
+            localStorage.setItem("previousSearches", JSON.stringify(previousSearch));
+            renderHistory();
+
             response.json().then(function (data) {
-                console.log(data);
+                renderCurrent(data.current.temp, data.current.wind_speed, data.current.humidity, data.current.uvi, data.current.weather[0].icon);
+                renderForecast(data.daily.slice(0, 4));
             });
         } else {
             console.log("Error" + response.statusText);
@@ -41,31 +53,35 @@ function getWeatherData(lat, lon) {
     });
 };
 
+function renderCurrent(temp, wind, humidity, uv, icon) {
 
 
+}
 
+function renderForecast(dailyArray) {
 
+}
 
+function renderHistory() {
+    previousSearch = JSON.parse(localStorage.getItem("previousSearches"));
+    console.log(previousSearch);
+}
 
-
-
-
-// if valid response received from AP
-    // previousSearch.push(searchTerm);
-    // send to localStorage
-    // reder history list
-
-
-
-// render historyList function 
-// pull from local storage
 
 // init
+function init(){
     //render historyList
+    today = new Date();
+    let dd = String(today.getDate()).padStart(2, "0");
+    let mm = String(today.getMonth() + 1).padStart(2, "0");
+    let yyyy = today.getFullYear();
+    today = mm + "/" + dd + "/" + yyyy;
+    previousSearch = JSON.parse(localStorage.getItem("previousSearches"));
+}
 
 document.querySelector("#search-form").addEventListener("submit", function(event){
     event.preventDefault();
-    let searchTerm = document.querySelector("#search-term").value;
+    searchTerm = document.querySelector("#search-term").value;
     if (searchTerm) {
         let adjustedSearch = searchTerm.replace(" ", "+");
         getGeoData(adjustedSearch);
@@ -74,3 +90,5 @@ document.querySelector("#search-form").addEventListener("submit", function(event
     }
 
 })
+
+init();
